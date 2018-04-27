1ed92a2fcf7bb237638bcf7ff13996cd307ca085
zhanggy <441473064@qq.com>
2018-04-27 16:45:04 +0800

    no message

--- GitDemoTests/GitDemoTests.m
+++ GitDemoTests/GitDemoTests.m
@@ -31,22 +31,32 @@
 
 - (void)test_clone {
     NSError* error = nil;
-    [IVELocalFileManager deleteDataWithRelativePath:@"clone"];
-    NSString *path = [IVELocalFileManager createDirectoryWithRelativePath:@"clone"];
-    
-    GTRepository *repo = [GTRepository cloneFromURL:[NSURL URLWithString:@"https://github.com/DullDevil/RSADemo"] toWorkingDirectory:[NSURL fileURLWithPath:path] options:nil error:&error transferProgressBlock:^(const git_transfer_progress * _Nonnull progress, BOOL * _Nonnull stop) {
+    _gitPath = [IVELocalFileManager createDirectoryWithRelativePath:@"gitTest"];
+    if (![[NSFileManager defaultManager] fileExistsAtPath:_gitPath]) {
+        GTCredentialProvider *provider = [GTCredentialProvider providerWithBlock:^GTCredential * _Nullable(GTCredentialType type, NSString * _Nonnull URL, NSString * _Nonnull userName) {
+            GTCredential *cre = [GTCredential credentialWithUserName:@"xporter" password:@"12345678" error:NULL];
+            return cre;
+        }];
+        NSDictionary *cloneOptions = @{GTRepositoryCloneOptionsCredentialProvider: provider };
         
-    }];
+        _repo = [GTRepository cloneFromURL:[NSURL URLWithString:@"http://localhost:81/xporter/test.git"] toWorkingDirectory:[NSURL fileURLWithPath:_gitPath] options:cloneOptions error:&error transferProgressBlock:nil];
+    } else {
+        _repo = [GTRepository initializeEmptyRepositoryAtFileURL:[NSURL fileURLWithPath:_gitPath] options:nil error:&error];
+    }
+    
     NSString *des = [NSString stringWithFormat:@"clone失败: \n %@",error];
-    NSAssert(repo, des);
+    NSAssert(_repo, des);
+  
 }
 
 - (void)test_initRepo {
     _gitPath = [IVELocalFileManager createDirectoryWithRelativePath:@"gitTest"];
     NSError* error = nil;
+    NSDictionary *option = @{GTRepositoryInitOptionsInitialHEAD:@"master"};
+    
     _repo = [[GTRepository alloc] initWithURL:[NSURL fileURLWithPath:_gitPath] error:&error];
     if (!_repo) {
-        _repo = [GTRepository initializeEmptyRepositoryAtFileURL:[NSURL fileURLWithPath:_gitPath] options:nil error:&error];
+        _repo = [GTRepository initializeEmptyRepositoryAtFileURL:[NSURL fileURLWithPath:_gitPath] options:option error:&error];
     }
     NSString *des = [NSString stringWithFormat:@"创建repo失败: \n %@",error];
     NSAssert(_repo,des);
@@ -65,40 +75,43 @@
     NSString *filePath = [NSString stringWithFormat:@"%@/%@",_gitPath,fileName];
     [string writeToFile:filePath atomically:YES encoding:NSUTF8StringEncoding error:&error];
     
-
     NSString *des = [NSString stringWithFormat:@"写入数据失败: \n %@",error];
     NSAssert(error == nil,des);
     
     GTReference *ref = [_repo headReferenceWithError:&error];
-    des = [NSString stringWithFormat:@"获取ref失败: \n %@",error];
-    NSAssert(error == nil,des);
-    
     NSMutableArray *parents = [NSMutableArray array];
     // 如果是第一次提交的话不需要 parents
     if (ref) {
         [parents addObject:ref.resolvedTarget];
     }
+    
+    error = nil;
     GTIndex *index = [_repo indexWithError:&error];
     des = [NSString stringWithFormat:@"获取index失败: \n %@",error];
     NSAssert(error == nil,des);
     
+    error = nil;
     [index addFile:fileName error:&error];
     des = [NSString stringWithFormat:@"添加文件失败: \n %@",error];
     NSAssert(error == nil,des);
     
+    error = nil;
     [index write:&error];
     des = [NSString stringWithFormat:@"写入文件失败: \n %@",error];
     NSAssert(error == nil,des);
     
+    error = nil;
     GTTree *tree = [index writeTree:&error];
     des = [NSString stringWithFormat:@"读取tree失败: \n %@",error];
     NSAssert(error == nil,des);
     
+    error = nil;
     GTSignature *sign = [[GTSignature alloc] initWithName:@"Test" email:@"441473064@qq.com" time:[NSDate date]];
     [_repo createCommitWithTree:tree message:string author:sign committer:sign parents:parents updatingReferenceNamed:@"HEAD" error:&error];
     des = [NSString stringWithFormat:@"提交失败: \n %@",error];
     NSAssert(error == nil,des);
 }
+
 - (void)test_log {
     [self test_initRepo];
     GTReference *ref = [_repo headReferenceWithError:NULL];
@@ -115,13 +128,13 @@
     GTConfiguration *conf = [_repo configurationWithError:NULL];
     NSError *error = nil;
     if (conf.remotes.count == 0) {
-        [GTRemote createRemoteWithName:@"origin" URLString:@"http://admin@localhost:2018/r/Test.git" inRepository:_repo error:&error];
+        [GTRemote createRemoteWithName:@"origin" URLString:@"http://localhost:81/xporter/test.git" inRepository:_repo error:&error];
     }
     NSString *des = [NSString stringWithFormat:@"添加remote失败: \n %@",error];
     NSAssert(conf.remotes.count > 0,des);
 }
 - (void)test_fetchRemote {
-    [self test_initRepo];
+    [self test_addRemote];
     NSError *error = nil;
     GTConfiguration *conf = [_repo configurationWithError:NULL];
     NSAssert(conf.remotes.count > 0, @"没有remote");
@@ -135,32 +148,14 @@
     NSAssert(error == nil,des);
 }
 
-
-- (void)test_mergeRemoteToLocal {
-    [self test_initRepo];
-    NSError *error = nil;
-    GTConfiguration *conf = [_repo configurationWithError:NULL];
-    NSAssert(conf.remotes.count > 0, @"没有remote");
-    
-    NSArray *branchs = [_repo remoteBranchesWithError:&error];
-    NSAssert(branchs.count > 0, @"未找到相应分支");
-    
-    GTBranch *firstBaranch = [branchs firstObject];
-    
-    BOOL r = [_repo mergeBranchIntoCurrentBranch:firstBaranch withError:&error];
-    NSString *des = [NSString stringWithFormat:@"合并失败: \n %@",error];
-    NSAssert(r, des);
-}
-
-
 - (void)test_pushRemote {
     [self test_initRepo];
+    [self test_commit];
+    
     GTConfiguration *conf = [_repo configurationWithError:NULL];
     NSError *error = nil;
-    NSAssert(conf.remotes.count > 0, @"没有remote");
-    
     GTRemote *remote = [conf.remotes firstObject];
-
+    
     NSDictionary *pushOptions = [self remoteOptions];
     
     GTBranch *curretntBarch = [_repo currentBranchWithError:&error];
@@ -171,13 +166,29 @@
     NSAssert(r, des);
 }
 
+- (void)test_pull {
+    [self test_clone];
+    GTBranch *brach = [_repo currentBranchWithError:NULL];
+    GTConfiguration *conf = [_repo configurationWithError:NULL];
+    NSError *error = nil;
+    GTRemote *remote = [conf.remotes firstObject];
+    NSDictionary *pullOptions = [self remoteOptions];
+    BOOL r = [_repo pullBranch:brach fromRemote:remote withOptions:pullOptions error:&error progress:^(const git_transfer_progress * _Nonnull progress, BOOL * _Nonnull stop) {
+        
+    }];
+    
+    NSString *des = [NSString stringWithFormat:@"拉取失败: \n %@",error];
+    NSAssert(r, des);
+
+}
+
 - (NSDictionary *)remoteOptions {
     GTCredentialProvider *provider = [GTCredentialProvider providerWithBlock:^GTCredential * _Nullable(GTCredentialType type, NSString * _Nonnull URL, NSString * _Nonnull userName) {
-        GTCredential *cre = [GTCredential credentialWithUserName:@"admin" password:@"admin" error:NULL];
+        GTCredential *cre = [GTCredential credentialWithUserName:@"xporter" password:@"12345678" error:NULL];
         return cre;
     }];
     
-    NSDictionary *remoteOptions = @{GTRepositoryRemoteOptionsCredentialProvider: provider };
+    NSDictionary *remoteOptions = @{GTRepositoryRemoteOptionsCredentialProvider:provider};
     
     return remoteOptions;
 }
